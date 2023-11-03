6642a07a690d095b18112aa029b88bdd754e4db4
Adam Fanslau <adam.fanslau@gmail.com>
2023-11-03 14:49:02 +0000

    small fix

--- 99-bottles-of-beer/script.js
+++ 99-bottles-of-beer/script.js
@@ -2,9 +2,12 @@ let songLyrics = document.getElementById("lyrics");
 
 function generateVerse (noOfBottles) {
     let verse = ``;
-    if (noOfBottles > 1) {
+    if (noOfBottles > 2) {
         verse = `<li>${noOfBottles} bottles of beer on the wall, ${noOfBottles} bottles of beer</li>
                 <li>Take one down and pass it around, ${noOfBottles - 1} bottles of beer on the wall</li>`;
+    } else if (noOfBottles == 2) {
+        verse = `<li>2 bottles of beer on the wall, 2 bottles of beer</li>
+                <li>Take one down and pass it around, 1 bottle of beer on the wall</li>`;
     } else if (noOfBottles == 1) {
         verse = `<li>1 bottle of beer on the wall, 1 bottle of beer</li>
                 <li>Take one down and pass it around, no more bottles of beer on the wall</li>`;
